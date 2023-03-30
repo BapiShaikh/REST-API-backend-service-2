ea72c325d9ac58bfd809b6a9ba57e85cba1f2903
Bapi Shaikh <bapishaikh1148@gmail.com>
2023-03-30 17:32:49 +0530

    Added code for assignment-2

---
+++ connections/connectDB.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose")
+
+const main = async()=>{
+    mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
+
+}
+
+module.exports = main
---
+++ model/post.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    body: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    user: {
+        type: String,
+        required: true,
+    },
+});
+
+const posts = new mongoose.model("posts", postSchema);
+
+module.exports = posts
